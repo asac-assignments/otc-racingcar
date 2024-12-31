254ed1e691653a4ea68446d74179db1f07815708
youskim <usk05@naver.com>
2024-12-31 16:01:27 +0900

    feat: App클래스에 racing start, move, print 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import RacingCar from "./RacingCar.js";
+import ValidateInput from "./ValidateInput.js";
+
 class App {
-  async play() {}
+  #racing_cars = []
+
+  async play() {
+    await this.startRacing();
+    for (let i = 0; i < 15; i++) {
+      this.moveCars();
+    }
+    this.printCars();
+  }
+
+  printCars() {
+    this.#racing_cars.forEach(car => {
+      car.printMovDistance();
+    });
+  }
+  
+  moveCars() {
+    this.#racing_cars.forEach(car => {
+      car.move();
+    });
+  }
+
+  async startRacing() {
+    const input_cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const validateInput = new ValidateInput();
+    this.#racing_cars = validateInput.validateCarName(input_cars);
+  }
 }
 
 export default App;
