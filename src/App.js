c28bb1c7051561caddd3e053f9f83b3863c8055e
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-02 01:23:22 +0900

    FEAT: 횟수 입력

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import InputView from "../view/InputView.js";
 class App {
     async play() {
         try {
-            const carNames = InputView.getCarNames();
+            const carNames = await InputView.getCarNames();
+            const rounds = await InputView.getRounds();
         } catch (error) {
             console.error(error.message);
         }
--- view/InputView.js
+++ view/InputView.js
@@ -10,6 +10,17 @@ class InputView {
         }
         return input.split(",");
     }
+
+    static async getRounds() {
+        const input = await Console.readLineAsync(
+            "경주할 횟수(1이상 2의 53승 -1 이하 양의 정수): "
+        );
+        const rounds = Number(input);
+        if (!Number.isInteger(rounds) || rounds <= 0) {
+            throw new Error("[ERROR] 양의 정수를 입력해야 합니다.");
+        }
+        return rounds;
+    }
 }
 
 export default InputView;
