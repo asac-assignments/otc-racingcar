2fb84bf8a1e357693abf162e5293ae9a236ecf52
youskim <usk05@naver.com>
2025-01-02 10:16:24 +0900

    feat: 입력값 검증기능 추가

--- src/ValidateInput.js
+++ src/ValidateInput.js
@@ -1,20 +1,33 @@
 import RacingCar from "./RacingCar.js";
 
 class ValidateInput {
-
 	validateCarName(input_cars) {
 		const split_input_cars = input_cars.split(",");
 		const cars = [];
+		if (split_input_cars.length < 2) {
+			throw new Error("[ERROR] 2개 이상의 자동차를 설정해주세요.");
+		}
 
     for (let car of split_input_cars) {
+			if (car.length > 5) {
+				throw new Error("[ERROR] 자동차 이름은 5글자 이하로 설정해주세요.");
+			}
+			if (car.length == 0) {
+				throw new Error("[ERROR] 자동차 이름은 비어있으면 안됩니다.");
+			}
 			cars.push(new RacingCar(car));
     }
 		return cars;
   }
 
 	validationMathNum(num) {
+		if (Number.isNaN(Number(num))) {
+			throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+		}
 		return num;
 	}
+
+	
 }
 
 export default ValidateInput;
