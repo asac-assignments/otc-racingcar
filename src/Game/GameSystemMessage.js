d418a30565a536fb09640e8b8450c3d4d47acde7
KKangHHee <skh8609@gmail.com>
2025-01-02 11:51:14 +0900

    docs: 진행과정 간의 출력문 상수화

---
+++ src/Game/GameSystemMessage.js
@@ -0,0 +1,12 @@
+const GameSystemMessage = {
+  RACE_CAR_NAME_REQUEST:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  TRY_COUNT_REQUEST: "시도할 횟수는 몇 회인가요?\n",
+  RACE_START_MESSAGE: "실행 결과\n",
+  RACE_RESULT_MESSAGE: "최종 우승자 : ",
+
+  ERROR_MESSAGE_CAR_NAME: "[ERROR] 자동차 명명 규칙에 어긋납니다.",
+  ERROR_MESSAGE_TRY_COUNT: "[ERROR] 반복 횟수를 잘못 입력했습니다.",
+};
+
+export default GameSystemMessage;
