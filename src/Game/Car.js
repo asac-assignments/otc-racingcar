82f97264c25cf33d89487cd05ffc8293dd2353e5
KKangHHee <skh8609@gmail.com>
2025-01-02 16:21:21 +0900

    docs: car 주석 변경

--- src/Game/Car.js
+++ src/Game/Car.js
@@ -1,14 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-// 자동차 클래스
-// 이름과 해당 객체의 이동 수를 기록
+// Model
 class Car {
   #carName;
   #forward;
-
-  // 생성자 정의
   constructor(name) {
     this.#carName = name;
-    this.#forward = 0; //문자와 고민, 숫자가 더 범용성이 높을듯듯
+    this.#forward = 0;
   }
   get getForward() {
     return this.#forward;
@@ -22,7 +19,6 @@ class Car {
       this.#forward += 1;
     }
   }
-  // 현재 차의 이름과 전진 상태를 반환
   carState() {
     return `${this.#carName} : ${"-".repeat(this.#forward)}`;
   }
