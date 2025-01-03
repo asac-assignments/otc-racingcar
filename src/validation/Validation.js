1f5fa94d386ec5b0df62bc5cf9d1054b92dbe2fd
sebn <sebn1247@gmail.com>
2025-01-03 15:12:25 +0900

    feat(Validation): 차 이름 수 유효성 검사

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -3,6 +3,9 @@ class Validation {
         if(carNames.some(name => name.length > 5)){
             throw new Error('[ERROR] 자동차 이름은 5자 이하로 작성해주세요.')
         }
+        if(carNames.length <=1 ){
+            throw new Error('[ERROR] 자동차 이름을 1개 이상 작성해주세요.')
+        }
     }
 }
 
