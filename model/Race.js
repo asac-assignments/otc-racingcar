eb1ebc370dc32bcf39228bbdccad758660a59c03
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-06 01:00:31 +0900

    FEAT: race 진행

---
+++ model/Race.js
@@ -0,0 +1,38 @@
+import Car from "./Car";
+
+class Race {
+    #cars;
+
+    constructor(carNames) {
+        const uniqueCars = [...new Set(carNames)];
+        if (uniqueCars.length < 2) {
+            throw new Error("[ERROR] 두 대 이상의 자동차가 필요합니다.");
+        }
+        this.#cars = uniqueCars.map((name) => new Car(name));
+    }
+
+    proceed() {
+        this.#cars.forEach((car) => {
+            const randomNumber = Math.floor(Math.random() * 10);
+            car.move(randomNumber);
+        });
+    }
+
+    getCars() {
+        return this.#cars;
+    }
+
+    getWinners() {
+        let max = 0;
+        const winners = [];
+        for (const car in this.#cars) {
+            max = car.getPosition() > max ? car.getPosition() : max;
+        }
+        for (const car in this.#cars) {
+            if (car.getPosition() === max) winners.push(car.getName());
+        }
+        return winners;
+    }
+}
+
+export default Race;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import InputView from "../view/InputView.js";
+import Race from "../model/Race.js";
 
 class App {
     async play() {
         try {
+            // 입력
             const carNames = await InputView.getCarNames();
             const rounds = await InputView.getRounds();
+
+            // 경주 및 차 객체 생성
+            const race = new Race(carNames);
+
+            // 라운드 진행 및 결과 출력
+            for (let i = 0; i < rounds; i++) {
+                race.proceed();
+                OutputView.showRound(race.getCars());
+            }
+
+            // 승리자 출력
+            OutputView.showWinners(race.getWinners());
         } catch (error) {
             console.error(error.message);
         }
