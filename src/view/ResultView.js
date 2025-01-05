64b97e52447c550818ee36b80e71d2d9ed069360
Seoha <seohas0428@naver.com>
2025-01-05 20:08:13 +0900

    fix: ResultView 경기 결과를  출력 형식에 맞게 출력하도록 수정

--- src/view/ResultView.js
+++ src/view/ResultView.js
@@ -1,33 +1,27 @@
 import { Console } from "@woowacourse/mission-utils";
-class ResultView{
-    constructor(){
-       this. DASH= '-';
+class ResultView {
+  constructor() {
+    this.DASH = "-";
+  }
 
-    }
-     
-    makePositionString(car){
-        const positionString = this.DASH.repeat(car.position)
-        return `${car.name} : ${positionString}`
-        
-    }
-   displayPosition(car){
-        const positionString = this.makePositionString(car)
-        Console.print(positionString)
-        Console.print("\n")
-
-    }
-     displayAllPositions(round, cars){
-        round ===0 && Console.print("실행 결과:\n")
-        cars.forEach(car=>this.displayPosition(car));
-    }
-    displayWinner(winner){
-        Console.print(winner)
-    }
-    displayAllWinners(winners){
-        Console.print("최종 우승자:\n")
-        winners.forEach(winner=>this.displayWinner(winner))
-
-    }
-
+  makePositionString(car) {
+    const positionString = this.DASH.repeat(car.position);
+    return `${car.name} : ${positionString}`;
+  }
+  displayPosition(car) {
+    const positionString = this.makePositionString(car);
+    Console.print(positionString);
+  }
+  displayAllPositions(round, cars) {
+    round === 0 && Console.print("\n실행 결과");
+    cars.forEach((car) => this.displayPosition(car));
+    Console.print("\n")
+  }
+  displayWinner(winner) {
+    Console.print(winner);
+  }
+  displayAllWinners(winners) {
+    Console.print(`최종 우승자: ${winners.join(", ")}`);
+  }
 }
-export default ResultView
+export default ResultView;
